316a298cd1a19c2f3556d62c4135527c7d2fa706
Prince733 <62391809+Prince733@users.noreply.github.com>
2022-11-02 16:03:45 +0530

    Update backend-api.service.ts

--- src/app/backend-api.service.ts
+++ src/app/backend-api.service.ts
@@ -5,6 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class BackendApiService {
     URL: string ='http://localhost:8080';
+    URL2: string ='http://localhost:8081';
     constructor(private httpclient: HttpClient) { }
     registerUser(registerform: any) {
        return this.httpclient.post(`${this.URL}/register`,registerform,{
@@ -18,28 +19,28 @@ export class BackendApiService {
         })
     }
     createrooms(createdroom:any){
-        return this.httpclient.post(`${this.URL}/room`,createdroom,{
+        return this.httpclient.post(`${this.URL2}/room`,createdroom,{
             observe:'body'
         })
     }
     searchingrooms(searchingroomdetails:any){
-        return this.httpclient.get(`${this.URL}/rooms/search`,searchingroomdetails)
+        return this.httpclient.get(`${this.URL2}/rooms/search`,searchingroomdetails)
     }
     reservingroom(detailsOfRoomBooked:any){
-        return this.httpclient.post(`${this.URL}/reservation`,detailsOfRoomBooked)
+        return this.httpclient.post(`${this.URL2}/reservation`,detailsOfRoomBooked)
     }
     reservations(){
-        return this.httpclient.get(`${this.URL}/reservations`,{
+        return this.httpclient.get(`${this.URL2}/reservations`,{
             observe: 'body'
         })
     }
     deleteroom(reservationId:string){
-        return this.httpclient.get(`${this.URL}/reservation/${reservationId}`,{
+        return this.httpclient.get(`${this.URL2}/reservation/${reservationId}`,{
             observe: 'body'
         })
     }
     logout(){
-        return this.httpclient.post(`${this.URL}/logout`,{
+        return this.httpclient.post(`${this.URL2}/logout`,{
             observe: 'body'
         })
     }
